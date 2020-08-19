5f698c236522e7badcddba19c66f7985a2988bbd
saraezzooff <saraezzooff@gmail.com>
2020-08-19 23:25:31 +0300

    Updates

---
+++ precache-manifest.fb640e3df376b5f4fdc5dbd1cdaee036.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94eef8fe6e3f9c371a9c5ae80f5573f",
+    "url": "/country_app/index.html"
+  },
+  {
+    "revision": "fbe6e4f5866dfc2bc40e",
+    "url": "/country_app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1616a5a6ee696376c963",
+    "url": "/country_app/static/js/2.61366f5a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/country_app/static/js/2.61366f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe6e4f5866dfc2bc40e",
+    "url": "/country_app/static/js/main.37299920.chunk.js"
+  },
+  {
+    "revision": "f37ec4b576c7bbf89298",
+    "url": "/country_app/static/js/runtime-main.72d5732d.js"
+  },
+  {
+    "revision": "b63ded907e0a6ad3a8429a9145cd66de",
+    "url": "/country_app/static/media/night.b63ded90.svg"
+  },
+  {
+    "revision": "0739871623f3917d1a7e1353f1a1a6cc",
+    "url": "/country_app/static/media/sun.07398716.svg"
+  }
+]);
